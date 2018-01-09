f10e7f62d05b8617d1e00b5209d8ee789cc601db
16000518 <16000518>
2018-01-09 09:49:33 +0100

    init

---
+++ src/main/java/maven_archetype_quickstart/App.java
@@ -0,0 +1,17 @@
+package maven_archetype_quickstart;
+
+/**
+ * Classe App qui sert à rien
+ *
+ */
+public class App 
+{
+	/**
+	 * Voici le main
+	 * @param args
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
